a6ece264c719eb044f36ad92df9541de15c32506
Petar Dragicevic <dragicevicpetar14@gmail.com>
2024-07-03 17:30:30 +0200

    [ADD] Task DTO, Controller, Repository

--- Model/Models/Zadatak.py
+++ Model/Models/Zadatak.py
@@ -1,5 +1,38 @@
 class Zadatak:
-    def __init__(self, tekst: str, uradjen: bool, muzicki_elementi: list):
+    def __init__(self, id : int ,tekst: str, uradjen: bool, muzicki_elementi: list):
+        self.id = id
         self.tekst = tekst
         self.uradjen = uradjen
         self.muzicki_elementi = muzicki_elementi
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        self._id = value
+
+    @property
+    def tekst(self):
+        return self.tekst
+
+    @tekst.setter
+    def tekst(self, value):
+        self.tekst = value
+
+    @property
+    def uradjen(self):
+        return self.uradjen
+
+    @uradjen.setter
+    def uradjen(self, value):
+        self.uradjen = value
+
+    @property
+    def muzicki_elementi(self):
+        return self.muzicki_elementi
+
+    @muzicki_elementi.setter
+    def muzicki_elementi(self, value):
+        self.muzicki_elementi = value
