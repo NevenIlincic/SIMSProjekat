0b7a401acdbfe0c0b3cb8ffe6fe75c683f00dfd2
MladenPetric <mpetric2004@gmail.com>
2024-07-03 23:04:03 +0200

    [FIX] AddMusicalPieceForm

--- View/MusicSupervisorView.py
+++ View/MusicSupervisorView.py
@@ -1,5 +1,6 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from View.AddMusicalPieceFormView import AddMusicalPieceWindow
 from View.GeneratedFiles.MusicSupervisorMenuGenerated import Ui_MainWindow
 from PyQt5.QtCore import pyqtSignal
 from Model.DTO.UserInformationsDTO import UserInformationsDTO
@@ -19,6 +20,7 @@ class MusicSupervisorWindow(QMainWindow,  Ui_MainWindow):
         #Function call
         self.pushButton.clicked.connect(self.logout)
         self.participant_button.clicked.connect(self.add_participant)
+        self.piece_button.clicked.connect(self.add_musical_piece)
 
     def show_informations(self):
         self.title_label.setText("Welcome back, "+self.supervisor_dto.ime+" "+self.supervisor_dto.prezime+"!")
@@ -31,6 +33,6 @@ class MusicSupervisorWindow(QMainWindow,  Ui_MainWindow):
         self.add_participant_window = AddParticipantWindow()
         self.add_participant_window.show()
 
-    # def add_musical_piece(self):
-    #     self.add_musical_piece_window = AddMusicalPieceWindow()
-    #     self.add_musical_piece_window.show()
+    def add_musical_piece(self):
+        self.add_musical_piece_window = AddMusicalPieceWindow()
+        self.add_musical_piece_window.show()
